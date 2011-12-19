88bd0362abe1ef039be1127471eae60785057201
Jonathan Howard <howard.jonathan@gmail.com>
2011-12-19 00:28:08 -0800

    hello world of mako

--- src/py/laptop/main.py
+++ src/py/laptop/main.py
@@ -1,5 +1,10 @@
 from bottle import route, run
+from mako.template import Template
+
 from laptop import model
+
+template_dir = "src/py/laptop/view"
+secret_key = "548e8ce6f94cb5a225c55fe5a91bc33b"
 
 @route('/')
 def home():
@@ -22,19 +27,37 @@ def index(name='World'):
   return '<b>Hello %s!</b>' % name
 
 # Main selection page for laptops
-@route('laptop')
+@route('/laptop')
 def laptop():
   # Get get params
   # Get Questions
   # Print from template
   return "laptop page" 
 
-@route('api/:version/:endpoint')
+@route('/api/:version/:endpoint')
 def api(version=None, endpoint=None):
   if version != '1.0':
     if endpoint == 'rank':
       return rank()
   return 'TODO send this to 500'
+
+@route('/admin')
+def admin_home():
+  template = Template(filename='%s/admin/home.mako' % template_dir)
+  return template.render()
+
+@route('/admin/product')
+def admin_product():
+  template = Template(filename='%s/admin/product.mako' % template_dir)
+  # Get product data
+  return template.render()
+
+@route('/admin/product/post', method="POST")
+def admin_product_post():
+  # Validate secret key
+  # Validate data
+  # Insert data, update session
+  return admin_product()
 
 # Import configuration params
 def bootstrap():
---
+++ src/py/laptop/view/admin/product.mako
@@ -0,0 +1,4 @@
+<h1>products</h1>
+<p>
+  what's the secret key?
+</p>
