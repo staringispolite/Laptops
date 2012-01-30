bcdbf12ded4cecd5f4f18338bb1e71ea5c084169
Jonathan Howard <howard.jonathan@gmail.com>
2012-01-30 00:04:19 -0800

    moved server code to laptop/releases/latest

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-PATH=$PATH:/home/ec2-user:/home/ec2-user/src/py:/Users/jhoward/Code/laptops/src/py
-PYTHONPATH=$PYTHONPATH:/home/ec2-user:/home/ec2-user/src/py:/Users/jhoward/Code/laptops/src/py/
+PATH=$PATH:/home/ec2-user/laptop/releases/latest/src/py:/Users/jhoward/Code/laptops/src/py
+PYTHONPATH=$PYTHONPATH:/home/ec2-user/laptop/releases/latest/src/py:/Users/jhoward/Code/laptops/src/py/
 export PATH
 export PYTHONPATH
 python src/py/laptop/main.py
